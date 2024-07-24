a4347dfd62150df121ca5563465e8a35f9929e9b
Patrick Chilaisha <Patrickchils@gmail.com>
2024-07-24 13:23:21 +0200

    applied changes

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-let launchDate = new Date("Nov 10, 2024 12:00:00").getTime();
-
-let timer = setInterval(tick, 1000);
-function tick (){
-    let now = new Date().getTime();
-    let t = launchDate - now;
-
-    if(t > 0){
-        let days = Math.floor(t / (1000 * 60 * 60 * 24));
-        if (days < 10) { days = "0" + days; }
-
-        let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        if (hours < 10) { hours = "0" + hours; }
-
-        let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-        if (mins < 10) { mins = "0" + mins; }
-
-        let secs = Math.floor((t % (1000 * 60 )) / 1000);
-        if (secs < 10) { secs = "0" + secs; }
-
-        let time = `${days} : ${hours} : ${mins} : ${secs}`;
-
-        document.querySelector('#count').innerText = time;
-    }
-}
 
 window.addEventListener('scroll', function(){
     const header = document.querySelector('header');
@@ -36,16 +11,32 @@ function toggleMenu(){
     navigation.classList.toggle('active');
 }
 
-let sky = document.getElementById("sky");
-let guy = document.getElementById("guy");
+
 let text = document.getElementById("text");
 let count = document.getElementById("count");
 
 
 window.addEventListener('scroll', function(){
     var value = window.scrollY;
-    sky.style.top = value * 0.5 + 'px';
-    guy.style.left = value * 0.5 + 'px';
     text.style.top = value * 1 + 'px'; 
     count.style.top = value * 0.9 + 'px';
 })
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+    
+    for(var i = 0; i < reveals.length; i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
